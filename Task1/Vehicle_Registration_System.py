798df98589871a0ce42a5bfd67ddc9e08a1b1903
MArney86 <matthew.h.arney.1986@gmail.com>
2024-07-22 18:22:35 -0500

    Commenting of added code

--- Task1/Vehicle_Registration_System.py
+++ Task1/Vehicle_Registration_System.py
@@ -4,29 +4,31 @@ class Vehicle:
             self.type = type
             self.owner = owner
             
-        def update_owner(self, new_owner):
-            if new_owner != self.owner:
-                self.owner = new_owner
+        def update_owner(self, new_owner): #added update_owner method
+            if new_owner != self.owner: #check that the owner isn't the already existing owner
+                self.owner = new_owner #change the owner to the one provided
+            else: #owner is same
+                 print("That owner is the same as the current owner. No updated done.") #notify user
         
-        def display_vehicle(self):
-            print(f"\nVehicle Registration Number: {self.registration_number}")
-            print(f"Vehicle Type: {self.type}")
-            print(f"Vehicle Owner: {self.owner}")
+        def display_vehicle(self): #added display method
+            print(f"\nVehicle Registration Number: {self.registration_number}") #print the registration number
+            print(f"Vehicle Type: {self.type}") #print the vehicle type
+            print(f"Vehicle Owner: {self.owner}") #print the vehicle owner
 
 vehicle_list = [Vehicle('456229931038', 'Sedan', 'John Smith'),
                 Vehicle('934567238771', 'Pickup', 'Julie Jones'),
                 Vehicle('688431289975', 'SUV', 'Jerrod Falkner'),
                 Vehicle('831264762186', 'Sedan', 'Jane Smith'),
-                Vehicle('721351651354', 'Sedan', 'Julie Jones')]
+                Vehicle('721351651354', 'Sedan', 'Julie Jones')] #List of vehicles
 
-print("Registered Vehicles:")
-for vehicle in vehicle_list:
-    vehicle.display_vehicle()
+print("Registered Vehicles:") #heading for the current list of vehicles
+for vehicle in vehicle_list: #iterate through the vehicle list
+    vehicle.display_vehicle() #call the iterated vehicle's display method
 
-print("\nChanging Vehicles' Owner to new owner Jerry Jones\n")
-for vehicle in vehicle_list:
-    vehicle.update_owner('Jerry Jones')
+print("\nChanging Vehicles' Owner to new owner Jerry Jones\n") #Heading to notify user of change to ownership
+for vehicle in vehicle_list: #iterate through the list of vehicles
+    vehicle.update_owner('Jerry Jones') #update the owner to the new owner
 
-print("\nNew Vehicle Registrations:")
-for vehicle in vehicle_list:
-    vehicle.display_vehicle()     
+print("\nNew Vehicle Registrations:") #Heading for updated list of Vehicles
+for vehicle in vehicle_list: #iterate through the list of vehicles
+    vehicle.display_vehicle() #call the iterated vehicle's display method
--- Task2/Event_Management_System_Enhancement.py
+++ Task2/Event_Management_System_Enhancement.py
@@ -2,10 +2,10 @@ class Event:
     def __init__(self, name, date):
         self.name = name
         self.date = date
-        self.participant_count = 0
+        self.participant_count = 0 #added participant count to the class
 
-    def add_participant(self):
-        self.participants += 1
+    def add_participant(self): #added add_participant method
+        self.participants += 1 #add one participant to the participant count
     
-    def get_participant_count(self):
-        return self.participant_count
+    def get_participant_count(self): #added get_participant_count method
+        return self.participant_count #return the value of participant_count
